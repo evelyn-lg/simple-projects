02994aca7eb4b2d8319f5df90a795d9594333c47
evelyn-lg <lazaro.evelyn@outlook.com>
2025-12-27 17:30:46 -0600

    contador de intentos

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -1,15 +1,19 @@
 '''La computadora elige un número aleatorio entre 1 y 100,
 y el usuario debe adivinarlo con pistas ("mayor" o "menor").
-Conceptos: random.randint(), bucles while, comparaciones.'''
+Conceptos: random.randint(), bucles while, comparaciones.
+- Cuenta los intentos en los que adivinaste
+'''
 
 import random
 
 LI = 1
 LS = 100
 s_num = random.randint(LI, LS)
+c = 0
 
 while True:
     u_num = int(input(f'Ingresa el número que crees que es (del {LI} al {LS}): '))
+    c += 1
     if u_num < LI or u_num > LS:
         print(f'El número está entre el {LI} y el {LS}')
     elif u_num < s_num:
@@ -18,5 +22,8 @@ while True:
         print('El número es menor')
     elif u_num == s_num:
         print(f'Felicidades, acertaste! el número es {s_num}')
+        print(f'Te tomó {c} intentos')
+        
         break
+    
         
