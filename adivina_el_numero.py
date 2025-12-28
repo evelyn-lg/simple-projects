307ace949620c7f1c134c1f89775394f5c56f918
evelyn-lg <lazaro.evelyn@outlook.com>
2025-12-27 18:10:31 -0600

    volver a jugar

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -1,34 +1,50 @@
-'''La computadora elige un número aleatorio entre 1 y 100,
+'''La computadora elige un número aleatorio entre 1 y 100, o el rango especificado,
 y el usuario debe adivinarlo con pistas ("mayor" o "menor").
-Conceptos: random.randint(), bucles while, comparaciones.
 - Cuenta los intentos en los que adivinaste
+- Solo permite números enteros y te dice si ingresaste algo no valido
+- Pregunta si quieres volver a jugar
 '''
 
 import random
 
 LI = 1
 LS = 100
-s_num = random.randint(LI, LS)
-c = 0
+repetir = True
 
-while True:
-    try:
-        u_num = int(input(f'Ingresa el número que crees que es (del {LI} al {LS}): '))
-    except ValueError:
-        print('Ingresa un valor válido: solo números enteros')
-        continue
+while repetir:
+    s_num = random.randint(LI, LS)
+    c = 0
+    print("\n" + "="*20)
+    print("¡ADIVINA EL NÚMERO!")
+    print("="*20)
+    print('Para debug, el número es: ', s_num)
     
-    c += 1
-    if u_num < LI or u_num > LS:
-        print(f'El número está entre el {LI} y el {LS}')
-    elif u_num < s_num:
-        print('El número es mayor')
-    elif u_num > s_num:
-        print('El número es menor')
-    elif u_num == s_num:
-        print(f'Felicidades, acertaste! el número es {s_num}')
-        print(f'Te tomó {c} intentos')
+    while True:
+        try:
+            u_num = int(input(f'Ingresa el número que crees que es (del {LI} al {LS}): '))
+        except ValueError:
+            print('Ingresa un valor válido: solo números enteros')
+            continue
         
-        break
+        c += 1
+        if u_num < LI or u_num > LS:
+            print(f'El número está entre el {LI} y el {LS}')
+        elif u_num < s_num:
+            print('El número es mayor')
+        elif u_num > s_num:
+            print('El número es menor')
+        elif u_num == s_num:
+            print(f'Felicidades, acertaste! el número es {s_num}')
+            print(f'Te tomó {c} intentos')
+            break
+    
+    # lower() convierte a minusculas y strip() elimina espacios
+    res_repetir = input('¿Quieres volver a jugar? (s = sí) ').lower().strip()
+    if res_repetir != 's':
+        print('\nGracias por jugar')
+        repetir = False
+
+        
+        
     
         
